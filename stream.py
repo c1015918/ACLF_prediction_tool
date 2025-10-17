613bdbadb70f96002046fb7872c5a4de24d1686d
c1015918 <chenjingyi@zju.edu.cn>
2025-10-17 19:12:41 +0800

    Update XGB model and streamlit script

--- stream.py
+++ stream.py
@@ -2,19 +2,11 @@ import streamlit as st
 import joblib
 import numpy as np
 import pandas as pd
-import shap
-import matplotlib.pyplot as plt
 
 # =========================
 # 1️⃣ 加载模型
 # =========================
-import os
-
-# 获取当前文件夹路径
-BASE_DIR = os.path.dirname(__file__)
-MODEL_PATH = os.path.join(BASE_DIR, "XGBmodel.pkl")
-
-# 加载模型
+MODEL_PATH = r"C:\Users\陈静仪\Desktop\RF_8features_Streamlit\XGBmodel.pkl"
 model = joblib.load(MODEL_PATH)
 
 # =========================
@@ -22,8 +14,9 @@ model = joblib.load(MODEL_PATH)
 # =========================
 feature_names = [
     'Antibiotics', 'Cerebral Failure', 'Circulatory Failure', 'HE',
-    'ALB', 'AST', 'Cr', 'Neutrophils', 'TBIL', 'PT'
+    'HDL-C', 'Cr', 'PT', 'Globulin', 'Neutrophils'
 ]
+
 # Streamlit 页面标题
 st.title("Prediction Tool for Nosocomial Infections in ACLF")
 
@@ -40,18 +33,18 @@ for feature in binary_features:
     user_input[feature] = 1 if choice == "Yes" else 0
 
 # 数值型特征
-numeric_features = ['ALB', 'AST', 'Cr', 'Neutrophils', 'TBIL', 'PT']
+numeric_features = ['HDL-C', 'Cr', 'PT', 'Globulin', 'Neutrophils']
 st.subheader("Numerical feature")
 default_values = {
-    'ALB': 35.0,
-    'AST': 30.0,
-    'Cr': 70.0,
-    'Neutrophils': 4.0,
-    'TBIL': 17.0,
-    'PT': 12.0
+    'HDL-C': 1.2,        # mmol/L
+    'Cr': 70.0,          # µmol/L
+    'PT': 12.0,          # 秒
+    'Globulin': 30.0,    # g/L
+    'Neutrophils': 4.0   # x10^9/L
 }
 for feature in numeric_features:
     user_input[feature] = st.number_input(f"{feature}:", value=default_values[feature])
+
 # =========================
 # 4️⃣ 预测按钮
 # =========================
@@ -67,17 +60,10 @@ if st.button("Predict"):
     class0_prob = predicted_proba[0] * 100  # 假设类别0 = 低风险
     class1_prob = predicted_proba[1] * 100  # 类别1 = 高风险
 
-# Display probability
+    # 显示概率
     st.write(f"**Probability of Infection:** {class1_prob:.1f}%")
 
-# Risk determination based on threshold
-    threshold = 0.395
+    # 风险判定（阈值）
+    threshold = 0.365
     risk = "High Risk" if class1_prob / 100 >= threshold else "Low Risk"
     st.write(f"**Risk Assessment (Threshold {threshold:.3f}):** {risk}")
-
-
-
- 
-
-    
-      
